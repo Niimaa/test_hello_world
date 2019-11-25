826d92e5c805ddfadc5a0a31ce167a43413a763e
Nima Afshar <nafs080@aucklanduni.ac.nz>
2019-11-26 09:14:57 +1300

    New function added to print name.

--- src/hello_world/main.py
+++ src/hello_world/main.py
@@ -1,6 +1,11 @@
 def say_hello():
     print("Hello World!")
-    
+
+
+def say_hello_name(name):
+    print("Hello", name)
+
 
 if __name__ == '__main__':
     say_hello()
+    say_hello_name('Nima')
